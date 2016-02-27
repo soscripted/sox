b8768adbe3c96895e602a597a0f5b40f5a43c830
somebody <ehern.lee@gmail.com>
2016-02-27 16:23:53 +1000

    Fix chat.js

--- chat/sox.chat.js
+++ chat/sox.chat.js
@@ -1,9 +1,8 @@
-//title change hacks: http://stackoverflow.com/a/2499119/3323231\
-http://stackoverflow.com/a/29540461/3323231
+//title change hacks: http://stackoverflow.com/a/2499119/3323231, http://stackoverflow.com/a/29540461/3323231
 $('#new_tab').on('click', function() {
     $('#new_tab').before($('<li/>'));
     $('#tabs').append($('<li/>'));
     $('#tabs:last').html('<iframe/>', {
         'src': 'http://chat.stackexchange.com/'
     });
-})
+});
