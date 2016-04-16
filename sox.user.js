d0c9bd14ed637a986f9b8a9d2c219d9df7ae254c
shu8 <shu8@users.noreply.github.com>
2016-04-16 19:51:36 +0100

    #54 resource URL

--- sox.user.js
+++ sox.user.js
@@ -21,7 +21,7 @@
 // @require      https://cdn.rawgit.com/camagu/jquery-feeds/master/jquery.feeds.js
 // @require      https://rawgit.com/soscripted/sox/dev/sox.helpers.js?v=1.0.4d
 // @require      https://rawgit.com/soscripted/sox/dev/sox.enhanced_editor.js?v=1.0.4a
-// @require      https://rawgit.com/soscripted/sox/dev/sox.features.js?v=1.0.4j
+// @require      https://rawgit.com/soscripted/sox/dev/sox.features.js?v=1.0.4k
 // @require      https://api.stackexchange.com/js/2.0/all.js
 // @resource     settingsDialog https://rawgit.com/soscripted/sox/dev/sox.dialog.html?v=1.0.4a
 // @resource     featuresJSON https://rawgit.com/soscripted/sox/dev/sox.features.info.json?v=1.0.4c
