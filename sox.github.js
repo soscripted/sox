4abc34021fa6b2f857eca81991612dacd342e454
enki <enki-code@users.noreply.github.com>
2016-05-07 22:14:23 -0700

    refactoring

--- sox.github.js
+++ sox.github.js
@@ -1,5 +1,32 @@
 (function(sox, $, undefined) {
     'use strict';
 
+    sox.github = {
+        init: function(version, manager) {
+            // auto-inject version number and environment information into GitHub issues
+            function inject() {
+                var $issue = $('#issue_body');
+                if ($issue.length) {
+                    $issue.prop('disabled', 'true');
+                    var issueText = $issue.text();
 
+                    issueText = issueText.replace('1.X.X', version); //inject the SOX version by replacing the issue template's placeholder '1.X.X'
+                    issueText = issueText.replace('Chrome/Tampermonkey', manager); //inject the SOX userscript manager+platfirm by replacing the issue template's placeholder 'Chrome/Tampermonkey'
+                    $('#issue_body').delay(500).text(issueText).removeAttr('disabled');
+                }
+            }
+
+            if (location.href.indexOf('/issues/new') > -1) {
+                inject();
+                return;
+            }
+
+            $(document).on('pjax:complete', function() {
+                if (location.href.indexOf('/issues/new') > -1) {
+                    inject();
+                    return;
+                }
+            });
+        }
+    };
 })(window.sox = window.sox || {},jQuery);
