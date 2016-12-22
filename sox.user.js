951d688d9463b02d0a44d9655dc6267967d41ec3
shu8 <shubjain01@gmail.com>
2016-12-22 18:40:19 +0000

    dev e

--- sox.user.js
+++ sox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stack Overflow Extras (SOX)
 // @namespace    https://github.com/soscripted/sox
-// @version      2.0.3 DEV d
+// @version      2.0.3 DEV e
 // @description  Extra optional features for Stack Overflow and Stack Exchange sites
 // @contributor  ᴉʞuǝ (stackoverflow.com/users/1454538/)
 // @contributor  ᔕᖺᘎᕊ (stackexchange.com/users/4337810/)
