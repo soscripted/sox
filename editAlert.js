16a306f23fe2624fd705c9c192f12426201ce0de
shu8 <shubjain01@gmail.com>
2017-01-07 16:34:50 +0000

    #215

--- editAlert.js
+++ editAlert.js
@@ -10,6 +10,15 @@ posts = [{ //check for edits, (closure, reopen, new answers if question)
     options: ['retag', 'edit', 'state change', 'new answers']
 }, {}, ...]
 
+/questions/ids filter: !SCam31W85iAdF11znRBpj2qWFPRJV_*8fTZTOPnclcMRL3Dxjmxr-5DJdNc07fPo
+/answers/ids filter:   !*IXxMt)cy3)mYENixz4JCogmlz1T(H0s*KHikYPCS4H3YAaTeot5E.A9HbhuHZ
+/posts/ids/revisions filter: !*K)GWE1gDcf3YaWY
+
+/posts/ids/revisions:
+    - to check for retag, check if items[0].last_tags exists
+    - to check whether body has been edited, check whether items[0].body exists
+    - if title changed, items[0].last_title will exist
+
 comments = [{
     postId: 12
     sitename: 'meta',
@@ -19,7 +28,18 @@ comments = [{
 
 - add helper function to add notification
 - add helper function to get required info from API
-- at page load, loop through postsToWatch and commentsToWatch and retrospectively add the fa-envelope class if it is added
+
+- to show in notification:
+    - site logo (favicon on left)
+    - delete button
+    - retag/edit/state change/new answer
+    - comment/post current score
+
+    - if retag: new tags
+    - if edit: edit comment
+    - if state change: new state (colour?)
+    - if new answer: time and user and score of latest answer, (how many new answers?), whether accepted
+    - if new comment: comment body? time, author, score
 
 NOTE: NEED TO MULTIPLY SE TIMES BY 1000!!
 */
@@ -49,6 +69,9 @@ NOTE: NEED TO MULTIPLY SE TIMES BY 1000!!
 
     var postsToWatch = JSON.parse(GM_getValue('sox-editNotification-postsToWatch', '[]')), //[{"type":"question","postId":"289490","sitename":"meta","lastCheckedTime":1483799031360,"options":["edit"],"lastCheckedState":"open"}],
         commentsToWatch = JSON.parse(GM_getValue('sox-editNotification-commentsToWatch', '[]')),
+        apiQuestionFilter = '!SCam31W85iAdF11znRBpj2qWFPRJV_*8fTZTOPnclcMRL3Dxjmxr-5DJdNc07fPo',
+        apiAnswerFilter = '!*IXxMt)cy3)mYENixz4JCogmlz1T(H0s*KHikYPCS4H3YAaTeot5E.A9HbhuHZ',
+        apiRevisionFilter = '!*K)GWE1gDcf3YaWY',
         options = {
             'retag': 'retag',
             'edit': 'edit',
@@ -62,19 +85,70 @@ NOTE: NEED TO MULTIPLY SE TIMES BY 1000!!
     $('.comments').before('<i title="watch for new comments" class="fa fa-envelope-o sox-notify-on-change sox-watch-comments"></i>');
     $('.post-menu').append('<span class="lsep"></span><i title="watch post for changes" class="fa fa-envelope-o sox-notify-on-change sox-watch-post"></i></a>');
 
+    //----------------------------------MAIN PART---------------------------------------//
     if (postsToWatch.length) { //make the envelope sign black if the post is already on the watch list
         $.each(postsToWatch, function(i, o) {
-            var $post = $('div[data-' + o.type + 'id="' + o.postId + '"]');
+            var currentType = o.type,
+                currentPostId = o.postId,
+                currentSitename = o.sitename,
+                currentLastCheckedTime = o.lastCheckedTime,
+                currentLastCheckedState = o.lastCheckedState,
+                currentLastCheckedAnswerIds = o.lastCheckedAnswerIds,
+                currentOptions = o.options; /*options = {
+                    'retag': 'retag',
+                    'edit': 'edit',
+                    'state change': 'stateChange',
+                    'new answers': 'newAnswers'
+                }*/
+
+            var $post = $('div[data-' + currentType + 'id="' + currentPostId + '"]');
             $post.find('.sox-watch-post').removeClass('fa-envelope-o').addClass('fa-envelope');
+            //TODO: do the API checking
+
+            /*
+            if currentLastCheckedTime > 15 minutes {
+                if options.newAnswers || options.stateChange {
+                    do API request to /questions or /answers
+                    newAnswerIds = FROM API
+                    newState = FROM API
+                    if options.newAnswers && !options.stateChange {
+                        check for whether new answerId exists, notification = 'new answer'
+                    } else if options.stateChange && !options.newAnswers {
+                        check for whether currentLastCheckedState is same, notification = 'state change'
+                    } else if options.stateChange && options.newAnswers {
+                        check for whether new answerId exists, and whether state is different, notification = 'state change and new answer'
+                    }
+                }
+                if options.edit || options.retag {
+                    do API requst to posts/id/revisions
+                    retag = TRUE/FALSE FROM API
+                    edit = TRUE/FALSE FROM API
+                    if options.retag {
+                        check for retag and edit comment, notification = 'edit and retag'
+                    } else {
+                        check for edit comment, notification = 'edit'
+                    }
+                }
+            }
+            */
         });
     }
     if (commentsToWatch.length) { //make the envelope sign black if the comments section is already on the watch list
         $.each(commentsToWatch, function(i, o) {
-            var $comments = $('comments-' + o.postId);
+            var currentPostId = o.postId,
+                currentSitename = o.sitename,
+                currentLastCheckedTime = o.lastCheckedTime,
+                currentLastCheckedCommentIds = o.lastCheckedCommentIds;
+
+            var $comments = $('comments-' + currentPostId);
             $comments.prev('.sox-watch-comments').removeClass('fa-envelope-o').addClass('fa-envelope');
+            //TODO: do the API checking
+
         });
     }
+    //----------------------------------/MAIN PART---------------------------------------//
 
+    //setup div that appears when you click on 'watch post' envelope
     $.each(options, function(text, id) { //create list items, in form <label><input type='checkbox' id='id'>text</label>
         $ul.append($('<li>').append($('<label>').append($('<input>', {
             'id': id,
@@ -99,6 +173,7 @@ NOTE: NEED TO MULTIPLY SE TIMES BY 1000!!
         $optionsDivSave = $('#sox-edit-notification-options-save'),
         $optionsDivCancel = $('#sox-edit-notification-options-cancel');
 
+    //all the handlers for clicking on watch icons, clicking save, clicking cancel, closing div if clicked outside
     $(document).on('click', '.sox-watch-comments', function() {
         var $post = $(this).parents('.question, .answer');
 
