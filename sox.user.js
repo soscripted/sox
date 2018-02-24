875ff0e5cc21c7f5638bc74e84b0227e5759065a
ᔕᖺᘎᕊ <shu8@users.noreply.github.com>
2018-02-24 20:39:26 +0000

    new dev

--- sox.user.js
+++ sox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stack Overflow Extras (SOX)
 // @namespace    https://github.com/soscripted/sox
-// @version      2.1.0
+// @version      2.1.0 DEV
 // @description  Extra optional features for Stack Overflow and Stack Exchange sites
 // @contributor  ᴉʞuǝ (stackoverflow.com/users/1454538/)
 // @contributor  ᔕᖺᘎᕊ (stackexchange.com/users/4337810/)
