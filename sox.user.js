0e79f811ae940b04eeef4fd9992205844a9739c7
ᔕᖺᘎᕊ <shu8@users.noreply.github.com>
2019-02-02 12:07:27 +0000

    editComment: make dialog delete button smaller

--- sox.user.js
+++ sox.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/soscripted/sox
 // @homepage     https://github.com/soscripted/sox
 // @homepageURL  https://github.com/soscripted/sox
-// @version      2.3.28 DEV
+// @version      2.3.29 DEV
 // @description  Extra optional features for Stack Overflow and Stack Exchange sites
 // @contributor  ᴉʞuǝ (https://stackoverflow.com/users/1454538/, https://github.com/mezmi)
 // @contributor  ᔕᖺᘎᕊ (https://stackexchange.com/users/4337810/, https://github.com/shu8)
@@ -175,7 +175,7 @@
     }
 
     //custom events....
-    sox.helpers.observe('.new_comment', () => { //custom event that triggers when a new comment appears/show more comments clicked; avoids lots of the same mutationobserver
+    sox.helpers.observe('.new_comment, .comment', () => { //custom event that triggers when a new comment appears/show more comments clicked; avoids lots of the same mutationobserver
       $(document).trigger('sox-new-comment');
       sox.debug('sox-new-comment event triggered');
     });
