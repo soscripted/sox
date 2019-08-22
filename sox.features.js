5df0d5926625a7cc40f089c780137af7c783509a
Double Beep <38133098+double-beep@users.noreply.github.com>
2019-08-22 20:00:47 +0300

    Fix bug with color answerer's comments feature
    This appears to be a bug caused by the new SE HTML which is fixed in this commit.

--- sox.features.js
+++ sox.features.js
@@ -185,11 +185,13 @@
         for (let i = 0; i < answerCells.length; i++) {
           const cell = answerCells[i];
           const answererID = cell.querySelector('.post-signature:nth-last-of-type(1) a').href.match(/\d+/)[0];
-          const commentUsers = [...cell.nextElementSibling.getElementsByClassName('comment-user')]
-            .filter(c => c.href && c.href.contains(`/users/${answererID}/`));
+          const comments = document.getElementsByClassName('comments')[i + 1];
+          const commentUsers = $(comments).find(".comment-user");
 
           for (let c = 0; c < commentUsers.length; c++) {
-            commentUsers[c].classList.add('sox-answerer');
+              if (commentUsers[c].href.contains(`users/${answererID}`)) {
+                  commentUsers[c].classList.add('sox-answerer');
+              }
           }
         }
       }
