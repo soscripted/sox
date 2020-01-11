92dfe049a5fa89463e0cc5a69188e55e1184d027
double beep <38133098+double-beep@users.noreply.github.com>
2020-01-11 18:30:21 +0200

    markEmployees: don't remove logos when DOM changes
    Specifically, when clicking 'show x more comments' or when a review post appears

--- sox.features.js
+++ sox.features.js
@@ -40,19 +40,21 @@
       const $icon = $('<svg aria-hidden="true" class="sox-markEmployees-logo sox-sprite svg-icon iconStackExchange" width="18" height="18" viewBox="0 0 18 18"><path d="M15 1H3a2 2 0 0 0-2 2v2h16V3a2 2 0 0 0-2-2zM1 13c0 1.1.9 2 2 2h8v3l3-3h1a2 2 0 0 0 2-2v-2H1v2zm16-7H1v4h16V6z"></path></svg>');
       $icon.css('color', $('.mod-flair').css('color'));
 
-      const anchors = [...document.querySelectorAll('.comment a, .deleted-answer-info a, .employee-name a, .user-details a, .question-summary .started a')].filter(el => {
-        return !el.parentElement.classList.contains('user-gravatar32') && el.href && el.href.contains('/users/');
-      });
-      const ids = [];
+      function getIds() {
+        const anchors = [...document.querySelectorAll('.comment a, .deleted-answer-info a, .employee-name a, .user-details a, .question-summary .started a')].filter(el => {
+          return !el.parentElement.classList.contains('user-gravatar32') && $(el).find('.sox-markEmployees-logo') && el.href && el.href.contains('/users/');
+        });
+        const ids = [];
 
-      for (let i = 0; i < anchors.length; i++) {
-        const idMatch = anchors[i].href.match(/(\d+)/);
-        if (idMatch && ids.indexOf(idMatch[1]) === -1) ids.push(idMatch[1]);
-      }
-      sox.debug('markEmployees user IDs', ids);
+        for (let i = 0; i < anchors.length; i++) {
+          const idMatch = anchors[i].href.match(/(\d+)/);
+          if (idMatch && ids.indexOf(idMatch[1]) === -1) ids.push(idMatch[1]);
+        }
+        sox.debug('markEmployees user IDs', ids);
 
-      for (let i = 1; i < Math.trunc(ids.length / 100) + 2; i++) {
-        apiCall(i, ids.slice(i * 100 - 100, i * 100), anchors);
+        for (let i = 1; i < Math.trunc(ids.length / 100) + 2; i++) {
+          apiCall(i, ids.slice(i * 100 - 100, i * 100), anchors);
+        }
       }
 
       function apiCall(page, ids, anchors) {
@@ -72,14 +74,18 @@
             if (!items[i].is_employee) continue;
 
             anchors.filter(el => el.href.contains(`/users/${userId}/`)).forEach(el => {
-              console.warn("adding logo to " + userId);
-              $(el).append($('<span/>', {
-                title: 'employee (added by SOX)',
-              }).append($icon.clone()));
+              if (!$(el).find('.sox-markEmployees-logo').length) {
+                $(el).append($('<span/>', {
+                  title: 'employee (added by SOX)',
+                }).append($icon.clone()));
+              }
             });
           }
         })
       }
+
+      $(document).on('sox-new-comment', getIds);
+      $(document).on('sox-new-review-post-appeared', getIds);
     },
 
     copyCommentsLink: function() {
