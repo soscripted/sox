7a98d4047f5f5f9827604ee776d14afe9e3a1c29
double-beep <doublebeep7@gmail.com>
2020-11-11 14:33:07 +0000

    Remove jQuery IV (sox.github.js)

--- sox.github.js
+++ sox.github.js
@@ -5,21 +5,17 @@
     init: function(version, handler) {
       // auto-inject version number and environment information into GitHub issues
       function inject() {
-        if (!sox.location.on('github.com/soscripted/sox') || location.href.includes('feature_request')) return;
-        const $issue = $('#issue_body');
-        if ($issue.length) {
-          $issue.prop('disabled', 'true');
-          const environmentText = `
-**Environment**
+        if (!sox.location.on('github.com/soscripted/sox') || sox.location.on('feature_request')) return;
+        const issue = document.querySelector('#issue_body');
+        if (!issue) return;
+        const environmentText = `**Environment**
 SOX version: ${version}
-Platform: ${handler}
-`;
+Platform: ${handler}`;
 
-          let issueText = $issue.text();
-          issueText = issueText.replace('**Environment**', environmentText); //inject environment details
-          issueText += '\n---\n\n### Features Enabled \n\n    ' + JSON.stringify(sox.settings.load());
-          $('#issue_body').delay(500).text(issueText).removeAttr('disabled');
-        }
+        let issueText = issue.value;
+        issueText = issueText.replace(/..Environment..\n.*?Tampermonkey\)/, environmentText); // inject environment details
+        issueText += '\n---\n\n### Features Enabled \n\n    ' + JSON.stringify(sox.settings.load());
+        issue.value = issueText;
       }
 
       $(document).on('pjax:complete', inject);
